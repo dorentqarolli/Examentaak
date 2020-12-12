2959bb3a1f94c34b57d1c4a4b2f5aa8387e177dd
dorentqarolli <72071316+dorentqarolli@users.noreply.github.com>
2020-12-13 00:27:37 +0100

    push11

--- functions.php
+++ functions.php
@@ -1,3 +1,18 @@
 <?php
-//FUNCTIONS_PAGINA Alle functies komen hier te staan
+function maakConnectie(){
+        //connectie met databank
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "dierenarts";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $database);
+
+        // Check connection
+        if ($conn -> connect_error) {
+            die("Connection failed: " . $conn -> connect_error);
+        }
+        return $conn;
+    }
 ?>
