1140f34a7047a1f798bd14c8e9d1534f6daf2bc3
dorentqarolli <72071316+dorentqarolli@users.noreply.github.com>
2020-12-13 14:01:31 +0100

    push15

--- functions.php
+++ functions.php
@@ -84,4 +84,138 @@ function maakConnectie(){
         return $arrDier;
 
     }
+	
+	//einde 2de push
+	//3de push
+	 function kiesDier($arrDier,$idCurrentDier){
+        $returnString = "<div class='row'>
+                <div class='col-12'>
+                    <div class='form-group'>
+                        <label for='idCurrentDier'>Kies een dier</label>
+                        <select class='form-control' id='idCurrentDier' name='idCurrentDier' onchange='this.form.submit()'>
+                            <option value=''>---NIEUW DIER---</option>";
+        foreach($arrDier as $key => $value){
+            $selected = NULL;
+            if($key == $idCurrentDier){
+                $selected = "SELECTED";
+            }
+              $returnString .="
+                            <option value='$key' $selected >{$value['naam']}</option>";
+        }
+        $returnString .= "
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <hr>";
+        return $returnString;
+    }
+	
+	//einde derde push
+	//vierde push
+	function formDier($arrDier,$idCurrentDier){
+        $returnString = NULL;
+        if($idCurrentDier != NULL){
+            $returnString = PHP_EOL . "
+            <div class='row'>
+                <div class='col-12'>
+                    <h2>Dier</h2>
+                </div>
+                <div class='col-6'>
+                    <div class='form-group'>
+                        <label for='naam'>naam</label>
+                        <input type='text' class='form-control' id='naam' name='naam' value='{$arrDier[$idCurrentDier]['naam']}'>
+                    </div>
+                </div>
+                <div class='col-12'>
+                    <h2>Eigenaars</h2>kehba
+                </div>
+                <div class='col-6'>
+                    <div class='form-group'>";
+                    foreach ($arrDier[$idCurrentDier]['eigenaars'] as $idEigenaar => $dataEigenaar) {
+                        $returnString .= "<label for='naamEigenaar'>Naam</label>
+                        <input type='text' class='form-control' id='eigenaar$idEigenaar' name='eigenaar$idEigenaar' value='{$dataEigenaar['naamEigenaar']}'>";
+                    };
+                $returnString .= "</div>
+                </div>
+                <div class='col-12'>
+                    <h2>Aandoening</h2>
+                </div>
+                <div class='col-6'>
+                    <div class='form-group'>";
+                    foreach ($arrDier[$idCurrentDier]['aandoeningen'] as $idAandoening => $dataAandoening) {
+                        $returnString .= "<label for='aandoening'>Aandoening</label>
+                        <input type='text' class='form-control' id='aandoening$idAandoening' name='aandoening$idAandoening' value='{$dataAandoening['aandoening']}'>
+                        <label for='beschrijvingAandoening'>Beschrijving</label>
+                        <input type='text' class='form-control' id='beschrijving$idAandoening' name='beschrijving$idAandoening' value='{$dataAandoening['beschrijvingAandoening']}'>";
+                    };
+                    $returnString .= "</div>
+                </div>
+                <div class='col-12'>
+                    <h2>Behandeling</h2>
+                </div>
+                <div class='col-10'>
+                    <div class='form-group'>";
+                    foreach ($arrDier[$idCurrentDier]['behandelingen'] as $idBehandeling => $dataBehandeling) {
+                        $returnString .= "<label for='datumBehandeling'>Datum</label>
+                        <input type='text' class='form-control' id='datum$idBehandeling' name='datum$idBehandeling' value='{$dataBehandeling['datumBehandeling']}'>
+                        <label for='behandeling'>Behandeling</label>
+                        <input type='text' class='form-control' id='behandeling$idBehandeling' name='behandeling$idBehandeling' value='{$dataBehandeling['behandeling']}'>";
+                    };
+                    $returnString .= "</div>
+                </div>
+            </div><hr>";
+            
+        }else{
+            $returnString = PHP_EOL . "<div class='row'>
+			<div class='col-12'>
+				<h2>Dier</h2>
+			</div>
+                <div class='col-6'>
+                    <div class='form-group'>
+                        <label for='naam'>naam</label>
+                        <input type='text' class='form-control' id='naam' name='naam' value=''>
+                    </div>
+                </div>
+            <div class='col-12'>
+                <h2>Eigenaar</h2>
+            </div>
+                <div class='col-6'>
+                    <div class='form-group'>";
+                    foreach ($arrDier[$idCurrentDier]['eigenaars'] as $idEigenaar => $dataEigenaar) {
+                        $returnString .= "<label for='naamEigenaar'>Naam</label>
+                        <input type='text' class='form-control' id='eigenaar$idEigenaar' name='eigenaar$idEigenaar' value=''>";
+                    };
+                $returnString .= "</div>
+                </div>
+            <div class='col-12'>
+                <h2>Aandoening</h2>
+            </div>
+                <div class='col-6'>
+                    <div class='form-group'>";
+                    foreach ($arrDier[$idCurrentDier]['aandoeningen'] as $idAandoening => $dataAandoening) {
+                        $returnString .= "<label for='aandoening'>Aandoening</label>
+                        <input type='text' class='form-control' id='aandoening$idAandoening' name='aandoening$idAandoening' value=''>
+                        <label for='beschrijvingAandoening'>Beschrijving</label>
+                        <input type='text' class='form-control' id='beschrijving$idAandoening' name='beschrijving$idAandoening' value=''>";
+                    };
+                    $returnString .= "</div>
+                </div>
+            <div class='col-12'>
+                <h2>Behandeling</h2>
+            </div>
+                <div class='col-10'>
+                    <div class='form-group'>";
+                    foreach ($arrDier[$idCurrentDier]['behandelingen'] as $idBehandeling => $dataBehandeling) {
+                        $returnString .= "<label for='datumBehandeling'>Datum</label>
+                        <input type='text' class='form-control' id='datum$idBehandeling' name='datum$idBehandeling' value=''>
+                        <label for='behandeling'>Behandeling</label>
+                        <input type='text' class='form-control' id='behandeling$idBehandeling' name='behandeling$idBehandeling' value=''>";
+                    };
+                    $returnString .= "</div>
+            </div><hr>";
+        }
+        return $returnString;
+    }
+	//einde vierde push
 ?>
