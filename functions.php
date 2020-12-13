aeca940d402dd11c838a1be6db2b57732ae5a16a
dorentqarolli <72071316+dorentqarolli@users.noreply.github.com>
2020-12-13 03:04:02 +0100

    push14

--- functions.php
+++ functions.php
@@ -15,4 +15,73 @@ function maakConnectie(){
         }
         return $conn;
     }
+	
+	function maakArray($conn){
+        //data selecteren
+        $sql = "SELECT * FROM dieren";
+        $result = $conn->query($sql);
+        $arrDier = array();
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                $arrDier[$row["id"]]['naam'] = $row["naam"];
+                $sqlEigenaar = "SELECT 
+                eigenaars.id as idEigenaar,
+                eigenaars.volldeigenaam as naamEigenaar
+                FROM eigenaar_dier
+                INNER JOIN eigenaars 
+                ON eigenaar_dier.id_eigenaar=eigenaars.id
+                WHERE eigenaar_dier.id_dier =".$row["id"];
+
+                $sqlAandoening = "SELECT 
+                aandoeningen.id as idAandoening,
+                aandoeningen.aandoening as aandoening,
+                aandoeningen.beschrijving as beschrijvingAandoening
+                FROM aandoening_dier
+                INNER JOIN aandoeningen
+                ON aandoening_dier.id_aandoening=aandoeningen.id
+                WHERE aandoening_dier.id_dier =".$row["id"];
+				
+                $sqlBehandeling = "SELECT
+                behandelingen.id as idBehandeling,
+                behandelingen.datum as datumBehandeling,
+                behandelingen.behandeling as behandeling
+                FROM aandoeningen
+                INNER JOIN behandelingen
+                ON aandoeningen.id=behandelingen.id_ziekte
+                WHERE behandelingen.id_dier =".$row["id"];
+                
+                $rstEigenaar = $conn->query($sqlEigenaar);
+                if ($rstEigenaar->num_rows > 0) {
+                    while($rowEigenaar = $rstEigenaar->fetch_assoc()) {
+                        $arrDier[$row["id"]]['eigenaars'][$rowEigenaar["idEigenaar"]] = array(
+                            "naamEigenaar" => $rowEigenaar["naamEigenaar"]);
+                    }
+                }
+
+                $rstAandoening = $conn->query($sqlAandoening);
+                if ($rstAandoening->num_rows > 0) {
+                    while($rowAandoening = $rstAandoening->fetch_assoc()) {
+                        $arrDier[$row["id"]]['aandoeningen'][$rowAandoening["idAandoening"]] = array(
+                            "aandoening" => $rowAandoening["aandoening"],
+                            "beschrijvingAandoening" => $rowAandoening["beschrijvingAandoening"]);
+                    }
+                }
+
+                $rstBehandeling = $conn->query($sqlBehandeling);
+                if ($rstBehandeling->num_rows > 0) {
+                    while($rowBehandeling = $rstBehandeling->fetch_assoc()) {
+                        $arrDier[$row["id"]]['behandelingen'][$rowBehandeling["idBehandeling"]] = array(
+                            "datumBehandeling" => $rowBehandeling["datumBehandeling"],
+                            "behandeling" => $rowBehandeling["behandeling"]);
+                    }
+                }
+            }
+            
+        } else {
+            echo "0 results";
+        }
+        return $arrDier;
+
+    }
 ?>
